666ea9282309f234a72a1f0a27089775e15db807
Luke Kacprowski <lukekac@me.com>
2026-01-30 13:19:28 -0500

    Error from comma
    There was an extra comma under the users table which caused an error when creating the table.

--- Server/create_tables.sql
+++ Server/create_tables.sql
@@ -7,8 +7,8 @@ CREATE TABLE IF NOT EXISTS users (
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     username VARCHAR(50) NOT NULL UNIQUE,   -- Unique username for login
-    password_hash VARCHAR(255) NOT NULL,     -- Store hashed passwords not original
-    token VARCHAR(255),                     -- Authentication token
+    password_hash VARCHAR(255) NOT NULL,    -- Store hashed passwords not original
+    token VARCHAR(255)                      -- Authentication token
 );
 
 -- Table to store contact information
