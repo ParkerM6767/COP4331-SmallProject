eade835c1d6cbaabf61e45d8b0ae3aee3a834747
insanefire10 <36336394+insanefire10@users.noreply.github.com>
2026-02-02 10:48:38 -0500

    started search function

--- Server/api/search.php
+++ Server/api/search.php
@@ -9,3 +9,29 @@
 *   ORDER BY first_name, last_name;
 *    ";
 */ 
+
+require '../components/db.php';
+header('Content-Type: application/json');
+
+# all if the user isnt actually searching (gets first 10 results) or contact_search if the user is actually searching
+$search_type = 'all';
+$offset = 0;
+$limit = 10;
+$uid = 1;
+
+try{
+    $search = $pdo->prepare("SELECT * FROM contacts WHERE user_id=? LIMIT ? OFFSET ?");
+    $search->execute([$uid, $limit, $offset]);
+    $rows = $search->fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode($rows);
+}
+catch(PDOException $e){
+    echo json_encode([
+        "error" => "Database error"
+    ]);
+}
+catch(Exception $e){
+    echo json_encode([
+        "error" => "Unknown error"
+    ]);
+}
