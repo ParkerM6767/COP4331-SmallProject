831b5a580246894e8959f5166bafd4b0e53a17ee
Alzapata317 <alzapata317@gmail.com>
2026-02-05 16:45:44 -0500

    added modal to contacts page for adding contacts, and made it responsive on mobile and ipad viewports

--- contact-manager/contacts/index.php
+++ contact-manager/contacts/index.php
@@ -1,5 +1,5 @@
 <?php
-include_once("../api/components/validatesession.php");
+// include_once("../api/components/validatesession.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,6 +8,7 @@ include_once("../api/components/validatesession.php");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contacts</title>
+    <link rel="stylesheet" href="../css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
@@ -15,13 +16,11 @@ include_once("../api/components/validatesession.php");
         crossorigin="anonymous"></script>
 </head>
 
-<body class="d-flex align-items-center justify-content-center vw-100 vh-100">
-    <nav class="navbar navbar-expand-lg vw-100 shadow"
-        style="position: fixed; top:0; background-color: rgb(44 103 237);">
+<body class="d-flex flex-column justify-content-center align-items-center vw-100 vh-100">
+    <nav class="navbar fixed-top navbar-expand-lg vw-100 shadow" style="background-color: rgb(44 103 237);">
         <div class="container-fluid">
-            <a class="navbar-brand text-white" href="#" id="userWelcome">Hello,
-                <?php echo $firstName . " " . $lastName; ?>!</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+            <!-- <a class="navbar-brand text-white" href="#" id="userWelcome">Hello,<?php echo $firstName . " " . $lastName; ?>!</a> -->
+            <button class="navbar-toggler mb-2" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -36,13 +35,35 @@ include_once("../api/components/validatesession.php");
             </div>
         </div>
     </nav>
+    <div class="modal" id="exampleModal" tabindex="-1">
+        <div class="modal-dialog d-flex justify-content-center align-items-center w-100 h-100">
+            <div class="modal-content w-sm-75 w-md-100 vh-50">
+            <div class="modal-header">
+                <h5 class="modal-title">Add Contact</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body d-flex flex-column justify-content-center px-5">
+                <label class="d-flex flex-column">First Name<input type="text"></label>
+                <label class="d-flex flex-column">Last Name <input type="text"></label>
+                <label class="d-flex flex-column">Email<input type="email"></label>
+                <label class="d-flex flex-column">Phone Number<input type="tel"></label>
+                <label class="d-flex flex-column">Work Number<input type="tel"></label>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save Contact</button>
+            </div>
+            </div>
+        </div>
+    </div>
     <container class="d-flex flex-column w-75 h-75 justify-content-center align-items-center p-4 border">
         <Nav class="w-100 d-flex flex-row mb-2">
             <input class="form-control w-75 me-3" placeholder="Search" />
-            <button class="btn btn-primary w-25">Add Contact</button>
+            <button class="btn btn-primary w-25 d-none d-md-block" data-bs-toggle="modal" data-bs-target="#exampleModal">Add Contact</button>
+            <button class="btn btn-primary w-25 d-block d-md-none fs-5" data-bs-toggle="modal" data-bs-target="#exampleModal">+</button>
         </Nav>
         <main class="d-flex flex-row w-100 h-100 border">
-            <aside class="w-25 d-flex flex-column h-100 border justify-content-between">
+            <aside class="w-sm-100 w-md-50 w-lg-25 d-flex flex-column h-100 border justify-content-between">
                 <div>
                     <div class="w-100 d-flex h-auto border-bottom">
                         <div id="profileIcon" class="ratio ratio-1x1 p-4 m-2 rounded-circle overflow-hidden shadow"
@@ -67,17 +88,14 @@ include_once("../api/components/validatesession.php");
                     </div>
                 </div>
                 <div class="d-flex w-100 justify-content-evenly p-3">
-                    <button class="btn btn-outline-primary">&lt;&lt;</button>
-                    <button class="btn btn-outline-primary">&lt;</button>
+                    <button class="d-none btn btn-outline-primary">&lt;</button>
                     <button class="btn btn-outline-primary">1</button>
                     <button class="btn btn-outline-primary">2</button>
                     <button class="btn btn-outline-primary">3</button>
-                    <button class="btn btn-outline-primary">...</button>
                     <button class="btn btn-outline-primary">&gt;</button>
-                    <button class="btn btn-outline-primary">&gt;&gt;</button>
                 </div>
             </aside>
-            <div class="w-75 d-flex p-5 flex-column gap-4 border">
+            <div class="d-none d-md-block w-75 d-flex p-5 flex-column gap-4 border">
                 <h1 class="d-flex flex-row justify-content-center">
                     <div class="me-3">Jake</div>
                     <div>Dove</div>
